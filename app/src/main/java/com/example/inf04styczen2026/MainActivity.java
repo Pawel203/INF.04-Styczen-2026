60f8eb2335e659b9c81961c46e489057c0e54703
Pablo <Pablo@DESKTOP-7I081SH>
2026-09-19 10:23:46 +0200

    Działajacy przycisk dalej.

--- app/src/main/java/com/example/inf04styczen2026/MainActivity.java
+++ app/src/main/java/com/example/inf04styczen2026/MainActivity.java
@@ -2,9 +2,9 @@ package com.example.inf04styczen2026;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,19 +13,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ImageView obrazek;
     RadioButton radio1, radio2, radio3;
     TextView pytanie;
+    RadioGroup odpowiedz;
     int[] zdjecia = {
             R.drawable.zad1,
             R.drawable.zad2,
             R.drawable.zad3
     };
-    int obecneZdjecie = 0;
-
+    String[] pytania = {
+            "Które to schronisko?",
+            "Zwierzę na zdjęciu to",
+            "W oddali są widoczne"
+    };
+    int[] poprawneOdpowiedzi = {
+            R.id.radio2,
+            R.id.radio1,
+            R.id.radio3
+    };
+    String[] odpowiedzi = {
+            "Na Rysiance.",
+            "Na Wielkiej Raczy.",
+            "Na Wielkiej Rycerzowej.",
+            "owczarek.",
+            "wilk.",
+            "kozica.",
+            "Himalaje.",
+            "Alpy.",
+            "Tatry."
+    };
+    int indexOdpowiedzi = 0;
+    int indexPytania = 0;
+    int wybranaOdpowiedz;
+    int liczbaPunktow = 0;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,16 +65,47 @@ public class MainActivity extends AppCompatActivity {
         radio1 = findViewById(R.id.radio1);
         radio2 = findViewById(R.id.radio2);
         radio3 = findViewById(R.id.radio3);
+        odpowiedz = findViewById(R.id.odpowiedz);
 
 
     }
     public void dalej(View view) {
-        if (obecneZdjecie>2){
-            obecneZdjecie = 0;
+        if (indexPytania == 2){
+            indexPytania = 0;
+            indexOdpowiedzi = 0;
         }
+
+        wybranaOdpowiedz = odpowiedz.getCheckedRadioButtonId();
+        if (wybranaOdpowiedz == poprawneOdpowiedzi[indexPytania]){
+            liczbaPunktow++;
+            indexOdpowiedzi +=3;
+            indexPytania ++;
+            obrazek.setImageResource(zdjecia[indexPytania]);
+            if (zdjecia[indexPytania] == R.drawable.zad2){
+                obrazek.setRotation(90f);
+            } else {
+                obrazek.setRotation(0f);
+            }
+            pytanie.setText(pytania[indexPytania]);
+            radio1.setText((odpowiedzi[indexOdpowiedzi]));
+            radio2.setText((odpowiedzi[indexOdpowiedzi+1]));
+            radio3.setText((odpowiedzi[indexOdpowiedzi+2]));
+        }
+
         else {
-            obecneZdjecie = 0;
+            indexPytania = 0;
+            indexOdpowiedzi = 0;
+            obrazek.setImageResource(zdjecia[indexPytania]);
+            if (zdjecia[indexPytania] == R.drawable.zad2){
+                obrazek.setRotation(90f);
+            } else {
+                obrazek.setRotation(0f);
+            }
+            pytanie.setText(pytania[indexPytania]);
+            radio1.setText((odpowiedzi[indexOdpowiedzi]));
+            radio2.setText((odpowiedzi[indexOdpowiedzi+1]));
+            radio3.setText((odpowiedzi[indexOdpowiedzi+2]));
         }
-        obrazek.setImageResource(zdjecia[obecneZdjecie]);
+        odpowiedz.clearCheck();
     }
 }
